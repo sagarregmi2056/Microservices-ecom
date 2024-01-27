2c76ba2debe0bf1d25eb913ea1a0128f718fca83
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-01-27 10:55:27 +0545

    serversetup/error handling

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,52 @@
-import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
-
 import dotenv from "dotenv";
 dotenv.config();
 
 const NODE_ENVIRONMENT = process.env.NODE_ENV;
-if (NODE_ENVIRONMENT === "developement")
-  // yadi node environment developement
-  dotenv.config({ path: ".envdev", override: true });
-else dotenv.config({ path: ".envprod", override: true });
 
-const port = process.env.PORT || 4000;
+switch (NODE_ENVIRONMENT) {
+  case "development":
+    // yadi node environment developement
+    dotenv.config({ path: ".envdev", override: true });
+    break;
+
+  case "staging":
+    dotenv.config({ path: ".envstaging", override: true });
+    break;
+
+  case "production":
+    dotenv.config({ path: ".envprod", override: true });
+    break;
+}
+
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+
+const { SERVER_PORT, HOST, LAMBDA } = process.env;
 
 import bodyParser from "body-parser";
 import { error } from "console";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
+app.options("*", cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
 
-// handing global error
+// if issue with any api related method or url
+app.use("/", async (req: any, res, next) => {
+  res.status(400).json({
+    message:
+      "issue with api Routes or method" + Date.now() + "" + NODE_ENVIRONMENT,
+  });
+});
+
+// handing global error like body and sytanxerror
 
 app.use(async (err: any, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof SyntaxError && "body" in err) {
@@ -35,8 +59,13 @@ app.use(async (err: any, req: Request, res: Response, next: NextFunction) => {
 process.on(
   "uncaughtException",
   (reason: Error, req: Request, res: Response) => {
-    console.log("uncaughtException");
-    console.log(reason);
+    const response = {
+      status: false,
+      message: "something went wrong try again later",
+      error: reason,
+    };
+
+    console.info(reason);
     process.exit(0);
   }
 );
@@ -44,14 +73,32 @@ process.on(
 process.on(
   "unhandledRejection",
   (reason: Error, req: Request, res: Response) => {
-    console.log("unhandledRejection");
-    console.log(reason);
-    process.exit(0);
+    const response = {
+      status: false,
+      message: "something went wrong try again later",
+      error: reason,
+    };
+
+    console.error(reason);
+    // process.exit(0);
 
     throw reason;
   }
 );
 
-app.listen(port, () => {
-  console.log(`server started at port ${port}`);
+app.use((req, res, next) => {
+  const response = {
+    status: false,
+    message: "page not found on the server/api not available",
+  };
 });
+
+if (LAMBDA == "false") {
+  app.listen(SERVER_PORT, () => {
+    console.log(
+      `server started at port ${HOST} ${SERVER_PORT} of ${NODE_ENVIRONMENT}} mode`
+    );
+  });
+} else {
+  module.exports = app;
+}
